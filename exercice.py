9b9d8aa3a187b4a0ab5ff99a8238d1667f553ac8
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-15 22:49:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,9 +4,9 @@
 
 def is_even_len(string: str) -> bool:
     if (len(string)%2 == 0):
-        return ("la chaine de caractère est paire")
+        return ("la chaîne de caractère est paire")
     else:
-        return ("la chaîne de caract")
+        return ("la chaîne de caractère est impaire")
 
 
 
