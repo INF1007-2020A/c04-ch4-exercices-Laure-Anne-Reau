c71f9b6cf91a60d503227604ef1444a4259c550c
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-15 22:52:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def remove_third_char(string: str) -> str:
     if len(string)<3 :
         return(string)
     else:
-        
+        return()
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
