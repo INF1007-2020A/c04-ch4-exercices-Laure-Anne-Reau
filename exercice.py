1490e6ab0a8d3a8020e85d7d77e484c8db06cf70
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-21 22:42:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -30,8 +30,9 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    nombre = sentence.count(word)
+    #nombre = sentence.count(word)
     #return nombre
+    
 
 def main() -> None:
     chaine = "Bonjour!"
