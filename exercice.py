d63fa5f0a816c24b311de9c75dda7d4e41efb1af
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-21 22:29:11 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,8 +21,10 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
+   
     for c in string :
-        if c 
+        if c == char :
+
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
